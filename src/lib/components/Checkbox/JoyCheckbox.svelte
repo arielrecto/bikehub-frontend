71e14e2c688498e2c5c3988cec5781ee832d0c82
davidrondina <rondinadavid.r@gmail.com>
2024-03-05 22:40:07 +0800

    chore: add checkbox component

---
+++ src/lib/components/Checkbox/JoyCheckbox.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { CheckboxSize, CheckboxVariant } from './types';
+
+	export let label: string = 'Checkbox label';
+	export let size: CheckboxSize = CheckboxSize.SM;
+	export let variant: CheckboxVariant | undefined = undefined;
+	export let formControl: boolean = true;
+
+	$: checkboxClass = `checkbox ${variant} ${size}`;
+</script>
+
+{#if formControl}
+	<div class="form-control w-52">
+		<label class="cursor-pointer label">
+			<span class="label-text">{label}</span>
+			<input type="checkbox" class={checkboxClass} />
+		</label>
+	</div>
+{:else}
+	<input type="checkbox" class={checkboxClass} />
+{/if}
